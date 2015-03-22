af9c6f644d01a4fce095dcdacd55d026f587f4f1
stnava <stnava@gmail.com>
2015-03-22 19:07:48 -0400

    COMP:...

---
+++ R/flags.R
@@ -0,0 +1,13 @@
+#' return ANTs commit information
+#'
+#' call this to get the ANTs commit
+#'
+#' @author Avants BB
+#' @examples
+#'
+#' antsCommit()
+#'
+#' @export antsCommit
+antsCommit <- function() {
+  "c8b9caccc428d4af24cc4ac061d59c8d41194cbb"
+}
